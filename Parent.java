12a729d166bfb98abbe7cee260a5a42c5e3e16ac
Rasyid Nugrahesa <100370094+rasyidnr@users.noreply.github.com>
2022-11-12 23:35:34 +0700

    Create Parent.java

---
+++ Parent.java
@@ -0,0 +1,26 @@
+package tugas2pbo; 
+  
+ public class Elemen { 
+   private String nama ; 
+  
+     Elemen (String nama) { 
+         this.nama = nama ;      
+     } 
+ 
+// ini method getter 
+     public String getNama (){ 
+         return this.nama; 
+     } 
+     public int getJamSibuk (){ 
+         return 0; 
+     } 
+ 
+// ini method setter 
+     public void setNama(String nama) { 
+         this.nama = nama ; 
+     } 
+  
+ // ini method umum 
+    public void display() { 
+     } 
+ }
